642e171e6d2222e3fba70961e8c8fce1ecd500f0
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-03-29 10:24:25 +0000

    Footer added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { current_year } from "../../helpers";
+
+//CSS Module
+import style from "./footer.module.css";
 
 const index = () => (
-  <div>
-    <p>Footer</p>
+  <div className={style.footer}>
+    <Link to="/" className={style.logo}>
+      <img src="/images/nba_logo.png" alt="nba logo" />
+    </Link>
+    <div className={style.right}>NBA {current_year}. All rights reserved</div>
   </div>
 );
 
